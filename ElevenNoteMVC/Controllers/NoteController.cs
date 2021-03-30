3504095c7c715c031e7ae04142aa4270eec41691
JoshuaCHartman <joshuachartman@gmail.com>
2021-03-29 22:47:43 -0400

    note create, success message, detial

--- ElevenNoteMVC/Controllers/NoteController.cs
+++ ElevenNoteMVC/Controllers/NoteController.cs
@@ -1,4 +1,6 @@
 ﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,8 +15,15 @@ namespace ElevenNoteMVC.Controllers
         // GET: Note
         public ActionResult Index()
         {
-            var model = new NoteListItem[0];
-            return View();
+            //var model = new NoteListItem[0];
+            //return View();
+
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            var model = service.GetNotes();
+
+            return View(model);
+
         }
 
         //GET
@@ -27,11 +36,40 @@ namespace ElevenNoteMVC.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-
+                return View(model);
             }
+
+            var service = CreateNoteService();
+
+            if (service.CreateNote(model))
+            {
+                //ViewBag.SaveResult = "Your Note was created.";
+                TempData["SaveResult"] = "Your Note was created.";
+
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Note could not be created.");
+
             return View(model);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
+
+            return View(model);
+        }
+
+
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
         }
     }
 }
